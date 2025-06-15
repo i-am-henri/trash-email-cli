476b8b217da6119c088fc4d385ca7c778e632023
i-am-henri <henri.gg@icloud.com>
2025-06-15 15:56:58 +0200

    add trash smtp server

--- packages/smtp/README.md
+++ packages/smtp/README.md
@@ -1,30 +1,3 @@
-**Haraka SMTP Server**
+**SMTP Server**
 
-This is an inbound-only SMTP Server, written in Typescript with Haraka. It's listening for incoming emails on port 25, checks if they are spam and sends them to an internal api (`/apps/api`).
-
-**Selfhosting**
-
-You can easily selfhost this smtp server on your vps. You need the port **25** open, you need to install `node-fetch` globally, a domain to receive emails and [bun](https://bun.sh/).
-
-1. Clone the entire repo.
-
-```bash
-git clone https://github.com/i-am-henri/trash-email-cli.git
-````
-
-2. Install the dependencies.
-
-```bash
-cd trash-email-cli
-bun install
-```
-
-3. Start the server. This will start the internal api server, the public server and the smtp server.
-
-```bash
-bun run server:dev
-```
-
-4. Forward the port 3000 with caddy or nginx. Keep the port 3001 closed.
-
-5. Edit the cli app and add your api url.
+This is the SMTP server for Trash Company, made with Haraka. To start, please create the "plugins" folder on the root of the smtp folder!
--- packages/smtp/global.d.ts
+++ packages/smtp/global.d.ts
@@ -1,28 +1,32 @@
-
 // globals.d.ts
+import { parse } from "node:path";
 import type { This } from "../../types/this.js";
 import type { Next } from "./types/parameter.ts";
 
 declare global {
-    var OK: undefined;
+	var OK: undefined;
+	var DENY: undefined;
 
-    // function, variables & objects which can be exported
-    interface Exports {
-        // Hooks
+	// function, variables & objects which can be exported
+	interface Exports {
+		// Hooks
 
-        // DATA hooks
-        hook_data: (this: This, next: Next, connection: any) => void;
-        hook_data_post: (this: This, next: Next, connection: any) => void;
+		// RCPT_TO hooks
+		hook_rcpt: (this: This, next: Next, connection: any) => void;
 
-        // QUEUE hooks
-        hook_queue: (this: This, next: Next, connection: any) => void
+		// DATA hooks
+		hook_data: (this: This, next: Next, connection: any) => void;
+		hook_data_post: (this: This, next: Next, connection: any) => void;
 
-        // plugin configuration
-        plugin: {
-            /**Name of this plugin. */
-            name: string;
-        };
-    }
+		// QUEUE hooks
+		hook_queue: (this: This, next: Next, connection: any) => void;
 
-    var exports: Exports;
+		// plugin configuration
+		plugin: {
+			/**Name of this plugin. */
+			name: string;
+		};
+	}
+
+	var exports: Exports;
 }
