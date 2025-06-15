8cb64a5a0337172f68a431569b474078873c60f1
i-am-henri <henri.gg@icloud.com>
2025-06-15 11:31:59 +0200

    feat(api & cli): add functioning cli, add websocket api

---
+++ apps/api/index.ts
@@ -0,0 +1,104 @@
+import { init } from "@paralleldrive/cuid2";
+import { serve } from "bun";
+import consola from "consola";
+import { z } from "zod";
+
+const mails = new Map<
+	string,
+	{
+		subject: string;
+		body: string;
+		from: string;
+	}[]
+>();
+
+const createId = init({
+	// the length of the id
+	length: 10,
+});
+
+// @ts-expect-error
+const server = serve<{ email: string }>({
+	port: 3000,
+	routes: {
+		"/health": () => {
+			return new Response("OK");
+		},
+		"/email/receive": {
+			POST: async (req) => {
+				const body = await req.json();
+
+				const parse = await z
+					.object({
+						subject: z.string(),
+						body: z.string(),
+						from: z.string().email(),
+						to: z.string().email().endsWith("@trash.company"),
+					})
+					.safeParseAsync(body);
+
+				if (!parse.success) {
+					return new Response(JSON.stringify(parse.error), {
+						status: 400,
+					});
+				}
+
+				const data = parse.data;
+
+				if (!mails.has(data.to)) {
+					return new Response("No address found", { status: 404 });
+				}
+
+				mails.get(data.to)?.push(data);
+
+				server.publish(
+					data.to,
+					JSON.stringify({
+						success: true,
+						receivedEmail: data,
+					}),
+				);
+
+				return new Response("OK");
+			},
+		},
+	},
+	fetch: (req) => {
+		const url = new URL(req.url);
+
+		if (url.pathname === "/listen") {
+			const emailID = createId();
+
+			mails.set(`${emailID}@trash.company`, []);
+
+			server.upgrade(req, {
+				data: {
+					email: `${emailID}@trash.company`,
+				},
+			});
+		}
+	},
+	websocket: {
+		// this is called when a message is received
+		async message(ws, message) {
+			console.log(`Received ${message}`);
+			// send back a message
+			ws.send(`You said: ${message}`);
+		},
+		async close(ws) {
+			mails.delete(`${ws.data.email}@trash.company`);
+		},
+		async open(ws) {
+			ws.subscribe(ws.data.email);
+
+			ws.send(
+				JSON.stringify({
+					success: true,
+					email: ws.data.email,
+				}),
+			);
+		},
+	},
+});
+
+consola.success(`Server running on http://localhost:${server.port}`);
--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,2 +1,93 @@
 // #usr/bin/env bun
 
+import { intro, log, outro, tasks } from "@clack/prompts";
+import boxen from "boxen";
+import chalk from "chalk";
+import { z } from "zod";
+
+// config
+const url = "http://localhost:3000";
+
+console.log("");
+intro("Welcome to the Trash Email CLI!");
+
+// checking api and listen for updates
+await tasks([
+	{
+		title: "Message API",
+		task: async (message) => {
+			const res = await fetch(`${url}/health`);
+			// Do installation here
+			return "API is running!";
+		},
+	},
+]);
+
+// client.ts
+
+const email = "user@example.com";
+const socket = new WebSocket(
+	`ws://localhost:3000/listen?email=${encodeURIComponent(email)}}`,
+);
+
+socket.addEventListener("open", () => {
+	log.info("Connected to the server!");
+});
+
+socket.addEventListener("message", async (event) => {
+	const parse = await z
+		.object({
+			success: z.boolean(),
+			email: z.string().optional(),
+			error: z.string().optional(),
+			receivedEmail: z
+				.object({
+					subject: z.string(),
+					body: z.string(),
+					from: z.string().email(),
+					to: z.string().email().endsWith("@trash.company"),
+				})
+				.optional(),
+		})
+		.safeParseAsync(JSON.parse(event.data));
+
+	if (!parse.success) {
+		log.error(
+			"The server responded with an invalid message! Please try again.",
+		);
+		process.exit(1);
+	}
+
+	if (parse.data.success !== true || parse.data.error !== undefined) {
+		log.error("The server responded with an error! Please try again.");
+		process.exit(1);
+	}
+
+	if (parse.data.email !== undefined) {
+		log.info(`Your email is ${parse.data.email}.`);
+	}
+
+	if (parse.data.receivedEmail !== undefined) {
+		const labelWidth = 9; // "Subject:" ist 8 Zeichen lang
+
+		const content = [
+			`${chalk.greenBright("Email Received")} ${parse.data.receivedEmail.from}`,
+			`${chalk.gray("Subject:")} ${parse.data.receivedEmail.subject}`,
+			`${chalk.gray("Body:")}    ${parse.data.receivedEmail.body}`,
+		].join("\n");
+
+		const box = boxen(content, {
+			padding: 1,
+			borderStyle: "round",
+			borderColor: "gray",
+			align: "left",
+		});
+
+		console.log(box);
+	}
+});
+
+socket.addEventListener("close", () => {
+	outro("Connection got closed! Bye!");
+	process.exit(0);
+});
